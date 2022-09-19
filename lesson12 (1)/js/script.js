8545fdbe8e8fe17880e7ba5698dbfee4786d672f
sarxankaar <110904508+sarxankaar@users.noreply.github.com>
2022-09-20 00:40:20 +0400

    Add files via upload

---
+++ lesson12 (1)/js/script.js
@@ -0,0 +1,18 @@
+// let num1 = 15
+
+// let num2 = 5
+
+
+// if (15%5){ 
+//     {
+//        console.log(num1 % num2);;
+//         }
+//          if (num1 % num2) {
+//       console.log(остаток);;
+//       }}
+for (var i = 0 ; i < 10; i++)
+{
+    if(i % 2 == 0 && i != 0)
+    console.log(i);}
+{
+}
